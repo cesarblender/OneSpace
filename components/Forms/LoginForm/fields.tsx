98ca604585444a91ea844f1808e2e96be616ce63
seokkuuu <seokkuuu>
2022-11-27 17:11:50 -0400

    fixed dependency problem

--- components/Forms/LoginForm/fields.tsx
+++ components/Forms/LoginForm/fields.tsx
@@ -1,46 +1,45 @@
-import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import { useI18next } from "gatsby-plugin-react-i18next";
-import GenderField from "../../GenderField";
-import BirthdayField from "../../BirthdayField";
-import { Inputs } from "./index";
-import { UseFormReturn } from "react-hook-form";
+export {}
+// import * as React from "react";
+// import TextField from "@mui/material/TextField";
+// import Stack from "@mui/material/Stack";
+// import GenderField from "../../GenderField";
+// import BirthdayField from "../../BirthdayField";
+// import { Inputs } from "./index";
+// import { UseFormReturn } from "react-hook-form";
 
-interface FieldsProps {
-  formHook: UseFormReturn<Inputs, any>;
-}
+// interface FieldsProps {
+//   formHook: UseFormReturn<Inputs, any>;
+// }
 
-interface textFields {
-  id: "email" | "password";
-  type?: undefined | "email" | "password";
-}
+// interface textFields {
+//   id: "email" | "password";
+//   type?: undefined | "email" | "password";
+// }
 
-const Fields: React.FC<FieldsProps> = ({ formHook }) => {
-  const { t } = useI18next();
-  const {
-    register,
-    formState: { errors },
-  } = formHook;
+// const Fields: React.FC<FieldsProps> = ({ formHook }) => {
+//   const {
+//     register,
+//     formState: { errors },
+//   } = formHook;
 
-  const textFields: textFields[] = [
-    { id: "email", type: "email" },
-    { id: "password", type: "password" },
-  ];
+//   const textFields: textFields[] = [
+//     { id: "email", type: "email" },
+//     { id: "password", type: "password" },
+//   ];
 
-  return (
-    <Stack spacing={2} sx={{ marginY: 2 }}>
-      {textFields.map((item, i) => (
-        <TextField
-          label={t(item.id)}
-          type={item.type}
-          key={i}
-          error={!!errors[item.id]}
-          {...register(item.id, { required: true })}
-        />
-      ))}
-    </Stack>
-  );
-};
+//   return (
+//     <Stack spacing={2} sx={{ marginY: 2 }}>
+//       {textFields.map((item, i) => (
+//         <TextField
+//           label={item.id}
+//           type={item.type}
+//           key={i}
+//           error={!!errors[item.id]}
+//           {...register(item.id, { required: true })}
+//         />
+//       ))}
+//     </Stack>
+//   );
+// };
 
-export default Fields;
+// export default Fields;
--- components/Forms/LoginForm/index.tsx
+++ components/Forms/LoginForm/index.tsx
@@ -1,93 +1,94 @@
-import * as React from "react";
-import Fields from "./fields";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { useMutation } from "@apollo/client";
-import { REGISTER_MUTATION } from "./mutation.gql";
-import dayjs from "dayjs";
-import { useI18next, Trans } from 'gatsby-plugin-react-i18next';
-import { useCookies } from "react-cookie";
-import Snackbar from "@mui/material/Snackbar";
-import Button from "@mui/material/Button";
-import MuiAlert, { AlertProps } from "@mui/material/Alert";
+export {}
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-  props,
-  ref
-) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+// import * as React from "react";
+// import Fields from "./fields";
+// import { SubmitHandler, useForm } from "react-hook-form";
+// import { useMutation } from "@apollo/client";
+// import { REGISTER_MUTATION } from "./mutation.gql";
+// import dayjs from "dayjs";
+// import { useCookies } from "react-cookie";
+// import Snackbar from "@mui/material/Snackbar";
+// import Button from "@mui/material/Button";
+// import MuiAlert, { AlertProps } from "@mui/material/Alert";
 
-export type Inputs = {
-  email: string;
-  password: string;
-};
+// const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+//   props,
+//   ref
+// ) {
+//   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+// });
 
-const LoginForm: React.FC = () => {
-  const form = useForm<Inputs>();
-  const [registerMutation, { loading, error, reset }] = useMutation(
-    REGISTER_MUTATION,
-    {
-      onError: (err) => {
-        console.error(err);
-      },
-    }
-  );
-  const { t, navigate } = useI18next();
-  const [_cookies, setCookie] = useCookies(["accessToken", "expiresIn"]);
+// export type Inputs = {
+//   email: string;
+//   password: string;
+// };
 
-  const onSubmit: SubmitHandler<Inputs> = async (form) => {
-    const { data, errors } = await registerMutation({
-      variables: {
-        email: form.email,
-        password: form.password,
-      },
-    });
+// const LoginForm: React.FC = () => {
+//   const form = useForm<Inputs>();
+//   const [registerMutation, { loading, error, reset }] = useMutation(
+//     REGISTER_MUTATION,
+//     {
+//       onError: (err) => {
+//         console.error(err);
+//       },
+//     }
+//   );
+//   const { t, navigate } = useI18next();
+//   const [_cookies, setCookie] = useCookies(["accessToken", "expiresIn"]);
 
-    if (!!error || !!errors) {
-      console.error(errors);
-      return;
-    }
+//   const onSubmit: SubmitHandler<Inputs> = async (form) => {
+//     const { data, errors } = await registerMutation({
+//       variables: {
+//         email: form.email,
+//         password: form.password,
+//       },
+//     });
 
-    setCookie("accessToken", data.login.accessToken, { path: "/" });
-    setCookie("expiresIn", data.login.expiresIn, { path: "/" });
+//     if (!!error || !!errors) {
+//       console.error(errors);
+//       return;
+//     }
 
-    navigate("/feed");
-  };
+//     setCookie("accessToken", data.login.accessToken, { path: "/" });
+//     setCookie("expiresIn", data.login.expiresIn, { path: "/" });
 
-  return (
-    <React.Fragment>
-      <form style={{ width: "100%" }} onSubmit={form.handleSubmit(onSubmit)}>
-        <Fields formHook={form} />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ marginTop: 2 }}
-          disabled={loading}
-        >
-          {!loading && <Trans i18nKey="continue" />}
-          {loading && <Trans i18nKey="loading" />}
-        </Button>
-        <Button
-          onClick={() => navigate("/auth/register")}
-          fullWidth
-          variant="text"
-          sx={{ marginY: 2 }}
-        >
-          <Trans i18nKey="not_registered" />
-        </Button>
-      </form>
-      <Snackbar
-        open={!!error}
-        autoHideDuration={6000}
-        onClose={reset}
-      >
-        <Alert onClose={reset} severity="error" sx={{ width: "100%" }}>
-          {error?.message}
-        </Alert>
-      </Snackbar>
-    </React.Fragment>
-  );
-};
+//     navigate("/feed");
+//   };
 
-export default LoginForm;
+//   return (
+//     <React.Fragment>
+//       <form style={{ width: "100%" }} onSubmit={form.handleSubmit(onSubmit)}>
+//         <Fields formHook={form} />
+//         <Button
+//           type="submit"
+//           fullWidth
+//           variant="contained"
+//           sx={{ marginTop: 2 }}
+//           disabled={loading}
+//         >
+//           {/* {!loading && <Trans i18nKey="continue" />} */}
+//           {/* {loading && <Trans i18nKey="loading" />} */}
+//         </Button>
+//         <Button
+//           onClick={() => navigate("/auth/register")}
+//           fullWidth
+//           variant="text"
+//           sx={{ marginY: 2 }}
+//         >
+//           {/* <Trans i18nKey="not_registered" /> */}
+//         </Button>
+//       </form>
+//       <Snackbar
+//         open={!!error}
+//         autoHideDuration={6000}
+//         onClose={reset}
+//       >
+//         <Alert onClose={reset} severity="error" sx={{ width: "100%" }}>
+//           {error?.message}
+//         </Alert>
+//       </Snackbar>
+//     </React.Fragment>
+//   );
+// };
+
+// export default LoginForm;
